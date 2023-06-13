3ac7c655a17c2c20f06fa960766eb95ed38e73fc
Mauro9104 <maur123sa@gmail.com>
2023-06-12 23:53:52 -0500

    feat: :construction: Se realiza los nuevos estilos a login create y profile, actividad solicitado por el profesor alejandro segura

---
+++ src/app/user/profile/profile.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { UserservicesService } from '../../../services/userservices.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+
+export class ProfileComponent {
+
+  selectFile!:File;
+  userProfile:any;
+  editValue:boolean = false;
+
+  editProfile(){
+    this.editValue = true;
+  }
+  
+  constructor(private UserService: UserservicesService){}
+
+  ngOnInit (){
+    this.getUser();
+  }
+
+  getUser(){
+     this.UserService.getUser()
+     .subscribe(
+      ( response:any) =>{
+       console.log("response:",response)
+       this.userProfile = response
+      },
+      (error)=>{
+        console.log("Error", error)
+      }
+    )
+  }
+  cancelUpdate(){
+this.editValue=false;
+   
+
+  }
+
+ 
+ 
+  onFileSelected(event:any){
+
+    this.selectFile = event.target.files[0];
+
+  }
+}
